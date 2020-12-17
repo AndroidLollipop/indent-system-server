462f6c7ba5e26c5757bcbe57b5e06b7d161d6f54
AndroidLollipop <obfuscated00143241131643657478@gmail.com>
2020-12-17 16:41:13 +0800

    tracking sockets

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const http = require("http");
 
+/*pending an actual database*/
+var dataStore = [{name: "Mandai Crematorium Indent", internalUID: 0, startDateTime: "01/04/2020 12:34", endDateTime: "01/04/2020 23:45", POC: "lmao", POCPhone: "999", origin: "Hell Camp", destination: "Hellish Camp", status: "Pending"}, {name: "Mandai Crematorium Indent", internalUID: 1, startDateTime: "01/04/2020 12:34", endDateTime: "01/04/2020 23:45", POC: "lmao", POCPhone: "999", origin: "Hell Camp", destination: "Hellish Camp", status: "Pending"}, {name: "Mandai Crematorium Indent", internalUID: 2, startDateTime: "01/04/2020 12:34", endDateTime: "01/04/2020 23:45", POC: "lmao", POCPhone: "999", origin: "Hell Camp", destination: "Hellish Camp", status: "Pending"}]
+var notificationsStore = [{title: "Mandai Crematorium Indent is now Pending", internalUID: 0}]
+/*end*/
+
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 
@@ -18,13 +23,17 @@ const io = require("socket.io")(server, {
 
 let interval;
 
+var sockets = []
+
 io.on("connection", (socket) => {
+  sockets.push(socket)
   console.log("New client connected");
   if (interval) {
     clearInterval(interval);
   }
   interval = setInterval(() => getApiAndEmit(socket), 1000);
   socket.on("disconnect", () => {
+    sockets = sockets.filter(s => s !== socket)
     console.log("Client disconnected");
     clearInterval(interval);
   });
